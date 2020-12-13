3542edf2d1c066944c91fcfd43e3fc15d0a4f0e2
Muhammad Hewedy <mhewedy@gmail.com>
2020-12-13 06:24:03 +0300

    fix - getting base from wolox-ci

--- vars/base.groovy
+++ vars/base.groovy
@@ -1,7 +1,7 @@
 def call(ObjectModel model, def nextClosure) {
     timeout(time: 600, unit: 'SECONDS') {
         wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-            nextClosure(model)
+            nextClosure()(model)
         }
     }
 }
