b7673e7c3da3227e812456855cd69894d44b557c
Muhammad Hewedy <mhewedy@gmail.com>
2020-12-17 11:56:32 +0300

    pass this

--- vars/docker_network.groovy
+++ vars/docker_network.groovy
@@ -1,27 +1,32 @@
 def call(ObjectModel model) {
 
-    return Network.create(model.services.size() > 0)
+    return Network.create(model.services.size() > 0, this)
 }
 
 class Network {
     String id
     boolean hasServices
+    def node
 
-    static Network create(boolean hasServices) {
+    static Network create(boolean hasServices, def node) {
+
+        println(node)
+        println(node.getClass())
+
         def networkId = UUID.randomUUID().toString()
 
         if (hasServices) {
             println("creating network: ${networkId}")
-            runShell "docker network create --name ${networkId}"
+            node.sh "docker network create --name ${networkId}"
         }
 
-        return new Network(id: networkId, hasServices: hasServices)
+        return new Network(id: networkId, hasServices: hasServices, node: node)
     }
 
     def remove() {
         if (hasServices) {
             println("removing network: ${id}")
-            runShell "docker network rm ${id}"
+            node.sh "docker network rm ${id}"
         }
     }
 
--- vars/runShell.groovy
+++
@@ -1,3 +0,0 @@
-def call(def args) {
-    sh args
-}
