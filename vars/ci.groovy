3c20ccbe8b86d5ae403976586a98181456ee79e7
mhewedy <mhewedy@elm.sa>
2022-10-29 16:40:41 +0300

    feat: handle .ci-pipeline not found

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,7 +1,14 @@
 def call(String yamlName) {
 
-    def yaml = readYaml file: yamlName
-    ObjectModel model = ObjectModel.load(yaml)
+    def yaml
 
+    try {
+        yaml = readYaml file: yamlName
+    } catch (error) {
+        println("cannot load .ci-pipeline.yaml make sure it is at the root of your source code; "+ error)
+        return
+    }
+
+    ObjectModel model = ObjectModel.load(yaml)
     base(model)
 }
--- vars/dockerRmi.groovy
+++ vars/dockerRmi.groovy
@@ -3,7 +3,7 @@ def call(def image) {
         sh "docker images --filter 'reference=${image}*' --format \"{{.Tag}} {{.Repository}}:{{.Tag}}\" | sort -n | awk '{ print \$2 }' | xargs --no-run-if-empty docker rmi"
 
     } catch (ignored) {
-        // this would make the entire popeline fail. We don't want that
+        // this would make the entire pipeline fail. We don't want that
         println ignored
     }
 }
